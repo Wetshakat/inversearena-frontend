94c43b9fbb5cdf3387d7c5f782e9b8efd71e5d75
JamesVictor-O <victorjames40v@gmail.com>
2026-01-24 16:01:34 +0100

    fix: error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className="flex-grow">
         <Hero />
-        <ProtocolSteps />
+        <Protocol />
         <WhyInverse />
         <YieldShowcase />
         <BottomCta />
