0e266c1885692127b7cfffe123c7f38815069b6c
JamesVictor-O <victorjames40v@gmail.com>
2026-01-26 13:54:59 +0100

    fix: fixed layout error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { WalletProvider } from "@/features/wallet/WalletProvider";
-
-import { Geist, Geist_Mono, Press_Start_2P } from "next/font/google";
 
 import { Geist, Geist_Mono, Press_Start_2P, Space_Grotesk } from "next/font/google";
 import "./globals.css";
